8e312b3d203b6f11119ea62ef6e20dca37453eef
Cameron Harp <charp5257@gmail.com>
2020-07-31 10:17:13 -0600

    added request a sign button

--- js/custom.js
+++ js/custom.js
@@ -23,6 +23,11 @@
             }
         });
 
+        // Modal
+        $('#signModal').on('shown.bs.modal', function () {
+          $('#signModal').trigger('focus')
+        });
+
         // Parallax Js
         function initParallax() {
           $('#home').parallax("50%", 50);
@@ -121,6 +126,73 @@
         // WOW Animation js
         new WOW({ mobile: false }).init();
 
+        // Send Sign
+        var signForm = document.getElementById("sign_form");
+        // var signButton = document.getElementById("my-sign-form-submit");
+        var signStatus = document.getElementById("my-sign-form-status");
+        var signNameField = document.getElementById("person_name");
+        var signPhoneField = document.getElementById("person_phone");
+        var signAddressField = document.getElementById("person_address");
+
+        function signSuccess() {
+          signForm.reset();
+
+          $('#signModal').modal('hide');
+        };
+    
+        function signError() {
+          signStatus.innerHTML = "Oops! Something went wrong, please try again.";
+        };
+    
+        // handle the form submission event
+    
+        signForm.addEventListener("submit", function(ev) {
+          ev.preventDefault();
+
+          var hasErrors = false;
+
+          if(signNameField.value === "") {
+            $(signNameField).addClass("is-required");
+            hasErrors = true;
+          }
+
+          if(signAddressField.value === "") {
+            $(signAddressField).addClass("is-required");
+            hasErrors = true;
+          }
+
+          if(hasErrors) {
+            return;
+          }
+
+          var data = new FormData(signForm);
+
+          ajax(signForm.method, signForm.action, data, signSuccess, signError);
+        });
+
+        $("#signModal").on("hidden.bs.modal", function () {
+          $(signNameField).removeClass("is-required");
+          $(signAddressField).removeClass("is-required");
+
+          signForm.reset();
+        });
+
+        $('#person_name').on('blur', function (e) {
+          e.preventDefault();
+
+          if(signNameField.value !== "") {
+            $(signNameField).removeClass("is-required");
+          }
+        });
+
+        $('#person_address').on('blur', function (e) {
+          e.preventDefault();
+
+          if(signAddressField.value !== "") {
+            $(signAddressField).removeClass("is-required");
+          }
+        });
+
         // Send Email
         var form = document.getElementById("contact_form");
         var button = document.getElementById("my-form-submit");
@@ -138,18 +210,63 @@
           nameField.disabled = true
           emailField.disabled = true
           messageField.disabled = true
-        }
+        };
     
         function error() {
           status.innerHTML = "Oops! There was a problem.";
-        }
-    
-        // handle the form submission event
+        };
     
         form.addEventListener("submit", function(ev) {
           ev.preventDefault();
+
+          var hasErrors = false;
+
+          if(nameField.value === "") {
+            $(nameField).addClass("is-required");
+            hasErrors = true;
+          }
+
+          if(emailField.value === "") {
+            $(emailField).addClass("is-required");
+            hasErrors = true;
+          }
+
+          if(messageField.value === "") {
+            $(messageField).addClass("is-required");
+            hasErrors = true;
+          }
+
+          if(hasErrors) {
+            return;
+          }
+
           var data = new FormData(form);
+
           ajax(form.method, form.action, data, success, error);
+        });
+
+        $('#email_name').on('blur', function (e) {
+          e.preventDefault();
+
+          if(nameField.value !== "") {
+            $(nameField).removeClass("is-required");
+          }
+        });
+
+        $('#email_from').on('blur', function (e) {
+          e.preventDefault();
+
+          if(emailField.value !== "") {
+            $(emailField).removeClass("is-required");
+          }
+        });
+
+        $('#email_message').on('blur', function (e) {
+          e.preventDefault();
+
+          if(messageField.value !== "") {
+            $(messageField).removeClass("is-required");
+          }
         });
 
         function ajax(method, url, data, success, error) {
